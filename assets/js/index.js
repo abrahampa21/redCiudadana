610cb360323ab9aa3fec80aa60fc203259631e51
Abraham Pech <abrahamayil415@gmail.com>
2026-05-19 18:56:53 -0600

    Estilos listos en web design junto con interactividad JS

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const register = document.getElementById("register");
+const login = document.getElementById("login");
+
+function showLogin(){
+    register.style.display = "none";
+    login.style.display = "block";
+}
