e9f49905e5d1e87ad89c38f3ac749d87ed42771f
Abraham Pech <abrahamayil415@gmail.com>
2026-06-26 19:04:09 -0600

    Añadiendo confirmación al momento de cerrar sesión

--- includes/sidebarCiudadano.php
+++ includes/sidebarCiudadano.php
@@ -55,7 +55,7 @@ $nombre_usuario = $_SESSION["nombre"];
             d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5-5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" />
         </svg>
         <!--Aquí se colocará el logout.php-->
-        <a href="..\src\logout.php">Cerrar sesión</a>
+        <a href="..\src\logout.php" onclick="return confirm('¿Estás seguro que deseas salir?');">Cerrar sesión</a>
       </div>
     </div>
   </aside>
