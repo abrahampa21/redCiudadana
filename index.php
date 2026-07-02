2a40853e84d54e6f17701f285f99c843b6047cf9
Abraham Pech <abrahamayil415@gmail.com>
2026-07-02 07:15:35 -0600

    Importando link js de bootstrap

--- index.php
+++ index.php
@@ -363,6 +363,7 @@ $conn->close();
   </div>
 
   <script src="assets/js/index.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <?php if ($toast): ?>
 
     <script>
