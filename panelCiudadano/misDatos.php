e292062d6c1ab6886e6c117f53586dcffb65c5d1
Abraham Pech <abrahamayil415@gmail.com>
2026-07-04 09:48:03 -0600

    Creación de sección misDatos.php en panel del ciudadano

---
+++ panelCiudadano/misDatos.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../assets/css/panelCiudadano.css" />
+  <link rel="icon" href="../src/img/icon-pages.jfif" />
+  <link href="../src/output.css" rel="stylesheet" />
+  <title>Panel del ciudadano</title>
+</head>
+<body>
+  <?php include_once "../includes/sidebarCiudadano.php"; ?>
+
+
+
+  <script src="../assets/js/panelCiudadano.js"></script>
+</body>
+
+</html>
