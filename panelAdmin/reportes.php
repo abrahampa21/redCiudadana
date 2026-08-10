d952b5d425ea1b6f58acb0560fd8c102a69e4f5c
Abraham Pech <abrahamayil415@gmail.com>
2026-08-10 16:26:20 -0600

    ultimos cambios

--- panelAdmin/reportes.php
+++ panelAdmin/reportes.php
@@ -33,8 +33,6 @@
             <option value="">Estado: Todos</option>
             <option>Pendiente</option>
             <option>En Proceso</option>
-            <option>Resuelto</option>
-            <option>Rechazado</option>
           </select>
           <select class="filter-select" id="f-cat" onchange="renderReportes()">
             <option value="">Categoría: Todas</option>
