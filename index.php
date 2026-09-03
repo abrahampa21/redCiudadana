1956b9659489a619fb73b7716b3c2a7ca4a4fb8a
Abraham Pech <abrahamayil415@gmail.com>
2026-09-03 09:27:52 -0600

    ssss

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ require_once __DIR__ .'.../includes/login.php';
   <script
     src="https://kit.fontawesome.com/e522357059.js"
     crossorigin="anonymous"></script>
-  <title>Inicio de Sesiónes</title>
+  <title>Inicio de Sesión</title>
 </head>
 
 <body>
